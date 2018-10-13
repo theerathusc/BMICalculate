016b6a6084eb8b14873232d7c468c3fa7706c1c9
theerathusc <theerathusc_@outlook.co.th>
2018-10-13 15:08:43 +0700

    commit1

---
+++ app/src/main/java/com/example/admin/bmicalculator/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.admin.bmicalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Toast t2 = Toast.makeText(this, "Hello Android", Toast.LENGTH_SHORT);
+        t2.show();
+
+        final Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        Button exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
